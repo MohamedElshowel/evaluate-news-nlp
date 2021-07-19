89ce49140c8029d32b45158cf3ee522f989ab05b
Mohamed Elshowel <mohamed.elshowel@gmail.com>
2021-07-20 00:29:20 +0200

    fix: Build ServiceWorker after calling the meaning service to avoid unhandled errors

--- src/server/index.js
+++ src/server/index.js
@@ -26,38 +26,38 @@ app.use(express.static('dist'))
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
+  // res.sendFile('dist/index.html')
+  res.sendFile(path.resolve('src/client/views/index.html'))
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function () {
-    console.log('Example app listening on port 8081!')
+  console.log('Example app listening on port 8081!')
 })
 
 app.get('/meaning', function (req, res) {
 
-    const formData = new FormData();
-    formData.append("key", process.env.API_KEY);
-    formData.append("txt", req.query.text);
-    formData.append("lang", "en");  // language code like: [en, es, fr, ... ]
+  buildServiceWorker();
 
-    const requestOptions = {
-        method: 'POST',
-        body: formData,
-        redirect: 'follow'
-    };
+  const formData = new FormData();
+  formData.append("key", process.env.API_KEY);
+  formData.append("txt", req.query.text);
+  formData.append("lang", "en");  // language code like: [en, es, fr, ... ]
 
-    fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
-        .then(response => ({
-            status: response.status,
-            body: response.json()
-        }))
-        .then(({ status, body }) => body.then(data => {
-            res.send(data);
-        }))
-        .catch(error => console.log('error', error));
+  const requestOptions = {
+    method: 'POST',
+    body: formData,
+    redirect: 'follow'
+  };
+
+  fetch("https://api.meaningcloud.com/sentiment-2.1", requestOptions)
+    .then(response => ({
+      status: response.status,
+      body: response.json()
+    }))
+    .then(({ status, body }) => body.then(data => {
+      res.send(data);
+    }))
+    .catch(error => console.log('error', error));
 
 })
-
-buildServiceWorker();
